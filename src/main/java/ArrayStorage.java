71e34307df1ccc25aec317a9e8803309852ce0f1
Yamatoirina <yamatoirina@gmail.com>
2021-03-11 09:55:29 +0200

    [HW-04] add implementation to the ArrayStorage methods

--- src/main/java/ArrayStorage.java
+++ src/main/java/ArrayStorage.java
@@ -1,30 +1,78 @@
+package main.java;
+
 /**
  * Array based storage for Employees
  */
 public class ArrayStorage {
     Employee[] storage = new Employee[10000];
 
+
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                storage[i] = null;
+            }
+        }
+        System.out.println("List is clear");
     }
+
 
     void save(Employee employee) {
+        if (storage.length == size()) {
+            System.out.println("No free slots");
+        } else {
+            storage[size()] = employee;
+        }
     }
 
+
     Employee get(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null && storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
+        System.out.println("No such employee " + uuid);
         return null;
     }
 
+
     void delete(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = storage[size() - 1];
+                storage[size() - 1] = null;
+            }
+        }
     }
+
+
 
     /**
      * @return array, contains only Employees in storage (without null)
      */
     Employee[] getAll() {
-        return new Employee[0];
+        Employee[] allEmployee = new Employee[size()];
+        if (size() != 0) {
+            for (int i = 0; i < size(); i++) {
+                allEmployee[i] = storage[i];
+            }
+        } else {
+            System.out.println("List is empty");
+        }
+        return allEmployee;
     }
 
+
     int size() {
-        return 0;
+        int countEmp = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                countEmp++;
+            } else {
+                break;
+            }
+        }
+        return countEmp;
     }
 }
